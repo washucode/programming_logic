8ed7991ff00ee953e6e1337151bd7c09fb20a4be
washucode <estherw8525@gmail.com>
2017-01-17 10:45:11 +0300

    ModifiedFile2

--- primes.py
+++ primes.py
@@ -1,15 +1,22 @@
 
 def all_prime(n):
 	for s in range (0,n+1):
-		gen= True
+		gen = True
 		for i in range(2,int(n**0.5)+1):
 			if s% i == 0:
 				gen = False
-			
+			elif s %2 ==0:
+				gen = False
+
+			elif s ==2:
+				gen = True
+			elif s in range(0,2):
+			  gen = False
 			else:
 				gen = True
-		if True:
-			print s
+		if gen ==True:
+		  print s
+all_prime(309)
 
 
 		
