e4b0bfd833314b48278f2954769b36e80aff9c9b
washucode <estherw8525@gmail.com>
2017-01-17 07:24:09 +0300

    Modified5(generating primes)

--- primes.py
+++ primes.py
@@ -2,13 +2,13 @@ def all_prime(n):
 	for s in range (0,n+1):
 		gen= True
 		for i in range(2,int(n**0.5)+1):
-			if s% i == 0:
+			if x% i == 0:
 				gen = False
 			
 			else:
 				gen = True
 		if True:
-			print s
+			print x
 
 
 		
