9a45033aeb84044758e26a4610611683726fe13e
washucode <estherw8525@gmail.com>
2017-01-17 05:02:38 +0300

    Modified 3(generating primes)

--- primes.py
+++ primes.py
@@ -1,18 +1,21 @@
 def all_prime(n):
-
-	for i in range(2,int(n**0.5)+1):
-		if x in range(0,2):
-			return False
-		elif x%i ==0:
-			return False
-		else:
-			return True
-print 2
-	
-def gen_prime(n):
 	for x in range (0,n+1):
-		if all_prime(n):
+		
+		for i in range(2,int(n**0.5)+1):
+			if x % i == 0:
+				return False
+			elif x==2:
+				return True
+			elif  x in range (0,2):
+				return False
+			else:
+				return True
+		if True:
 			print x
+
+		
+	
+
 
 
 		
--- test_primes.py
+++ test_primes.py
@@ -14,5 +14,33 @@ class test_case_primes(unittest.TestCase):
 	def test_negativeone_notprime(self):
 		self.assertFalse(all_prime(-1))
 
+	def test_negativetwo_notprime(self):
+		self.assertFalse(all_prime(-2))
+
+	def test_negativethree_notprime(self):
+		self.assertFalse(all_prime(-3))
+
+	def test_negativefour_notprime(self):
+		self.assertFalse(all_prime(-4))
+
+	def test_negativefive_notprime(self):
+		self.assertFalse(all_prime(-5))
+
+	def test_negativesix_notprime(self):
+		self.assertFalse(all_prime(-6))
+
+	def test_negativeseven_notprime(self):
+		self.assertFalse(all_prime(-7))
+
+	def test_negativeEight_notprime(self):
+		self.assertFalse(all_prime(-8))
+
+	def test_negativenine_notprime(self):
+		self.assertFalse(all_prime(-9))
+
+
+
+	
+
 if __name__== '__main__':
 	unittest.main()
