aafec0021aab207b2999bbe9dcf7e71e48d158fb
washucode <estherw8525@gmail.com>
2017-01-17 14:41:25 +0300

    This fizzbuzz

---
+++ data.py
@@ -0,0 +1,3 @@
+print type(10)
+print type([1,2,3,4])
+print type(False)
---
+++ fizzbuzz.py
@@ -0,0 +1,23 @@
+def fizz_buzz(n):
+  
+  p = str(n)
+  p = p.split()
+  sum= 0
+  for i in p:
+    int(i)
+    sum+=i
+    return t
+    
+  if t%3 ==0 :
+    return 'Fizz'
+  
+  elif n%5==0:
+    return 'Buzz'
+    
+  elif t%3==0 and n%5==0:
+    return 'FizzBuzz'
+    
+  else:
+    pass
+fizz_buzz (15)
+  
