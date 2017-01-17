2670853e16250216f6fe77a619c67fea4ee27fa0
washucode <estherw8525@gmail.com>
2017-01-17 03:33:36 +0300

    Modified Code(generate primes)

--- primes.py
+++ primes.py
@@ -1,18 +1,19 @@
 def all_prime(n):
+
 	for i in range(2,int(n**0.5)+1):
 		if x < 2:
 			return False
-		elif x ==2:
+		elif x == 2:
 			return True
 		elif x%i ==0:
 			return False
 		else:
 			return True
-
-
-for x in range (0,n+1):
-	if all_prime(n):
-		print x
+	
+def gen_prime(n):
+	for x in range (0,n+1):
+		if all_prime(n):
+			print x
 
 
 		
