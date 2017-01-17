f490bbec8f116589efc1f2fd9e3e72e06c3458f9
washucode <estherw8525@gmail.com>
2017-01-17 08:33:43 +0300

    ModifiedFile(Generating Primes)

--- primes.py
+++ primes.py
@@ -1,14 +1,14 @@
 def all_prime(n):
-	for x in range (0,n+1):
+	for s in range (0,n+1):
 		gen= True
 		for i in range(2,int(n**0.5)+1):
-			if x% i == 0:
+			if s% i == 0:
 				gen = False
 			
 			else:
 				gen = True
 		if True:
-			print x
+			print s
 
 
 		
