126e39f613b7389a5117aa3ef48de6a4b4e31a56
washucode <estherw8525@gmail.com>
2017-01-17 03:53:17 +0300

    modifiedtests(generate primes)

--- test_primes.py
+++ test_primes.py
@@ -7,7 +7,9 @@ class test_case_primes(unittest.TestCase):
 	
 	def test_one_not_prime(self):
 		self.assertFalse(all_prime(1))
-		
+
+	def test_zero_not_prime(self):
+		self.assertFalse(all_prime(0))
 
 if __name__== '__main__':
 	unittest.main()
